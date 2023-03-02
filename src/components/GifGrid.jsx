6555a9608383afa53db773261c526bb002e5f56a
Juan <juanlazarte231@gmail.com>
2023-03-02 11:07:29 -0300

    Primer commit

---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,39 @@
+import { GifGridItem } from "./GifGridItem";
+import { useFetchGifs } from "./hooks/useFetchGifs";
+
+
+export const GifGrid = ({category}) => {
+
+  const {images, isLoading} = useFetchGifs(category);
+
+  console.log(isLoading)
+
+
+  return (
+    <>
+        <h3>{category}</h3>
+        {
+            isLoading && (<h2>Cargando...</h2>)
+        }
+
+        <div className="card-grid">
+            {
+                images?.map((image) => (
+                    <GifGridItem 
+                    key={image.id}
+                    {...image}
+/*                     title={image.title}
+                    url={image.url} */
+                    
+                    />
+
+                ))
+            }
+            
+        </div>
+    </>
+  )
+}
+
+
+
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+export const getGifs =  async(category) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=tfhw9uluj5WsbYD1aNya8BIcPe0Mv7g5&q=${category}&limit=10`
+
+    const resp = await fetch(url);
+    const {data} = await resp.json();
+    
+    const gifs = data.map(img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }));
+
+    return(gifs)
+}
